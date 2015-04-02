e74c5cc18fb5fa407f8ce6fbfdcf42cd73760d86
fujijun <fujinuma5589@gmail.com>
2015-04-03 03:46:43 +0900

    forth commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true, length: { in: 6..24 }, if: :password
+  validates :password_confirmation, presence: true, if: :password
 end
