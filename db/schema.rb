363c6e255fe7ce7aa3a5474fee60a0f57db5a8d1
fujijun <fujinuma5589@gmail.com>
2015-04-03 16:05:26 +0900

    rails g scaffold comment page

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403070228) do
+ActiveRecord::Schema.define(version: 20150403070448) do
 
   create_table "attenders", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "event_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
